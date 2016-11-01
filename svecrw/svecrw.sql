703083a8d92a9fb62a2b5e8bdfd9dde2f0a1a849
Jagadeesh Dk <dkjagadeesh@gmail.com>
2016-11-01 23:05:58 +0530

    Updated the database for the new modules.

--- svecrw/svecrw.sql
+++ svecrw/svecrw.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 01, 2016 at 11:00 AM
+-- Generation Time: Nov 01, 2016 at 06:34 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -41,30 +41,52 @@ CREATE TABLE `achlist` (
 
 INSERT INTO `achlist` (`id`, `name`, `designation`, `dept`, `acheivement`, `public`) VALUES
 (13, 'Jaswanth', '', 'it', 'FDP', 'yes'),
-(16, 'dileep', '', 'IT', 'journal', 'yes');
+(16, 'dileep', '', 'IT', 'journal', 'yes'),
+(17, '123', '12312', 'IT', '123123', 'yes'),
+(18, 'asda', 'sadas', 'IT', 'asdsa', 'yes');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `feed`
+-- Table structure for table `events`
 --
 
-CREATE TABLE `feed` (
+CREATE TABLE `events` (
   `name` varchar(30) NOT NULL,
   `designation` varchar(30) NOT NULL,
-  `dept` varchar(30) NOT NULL,
-  `journal_name` varchar(30) NOT NULL,
-  `title` text NOT NULL,
-  `paperinfo` varchar(30) NOT NULL,
-  `year` int(11) NOT NULL
+  `funding_agency` varchar(50) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `date` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `feed`
+-- Dumping data for table `events`
 --
 
-INSERT INTO `feed` (`name`, `designation`, `dept`, `journal_name`, `title`, `paperinfo`, `year`) VALUES
-('Jaswanth', 'Assistant professor', 'IT', 'Springer', 'Cloud Computing', 'ISBN 11011', 2016);
+INSERT INTO `events` (`name`, `designation`, `funding_agency`, `title`, `date`) VALUES
+('Jaswanth', 'M.Tech', 'Skynet', 'CloudComputing', '12/10/2016');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expert`
+--
+
+CREATE TABLE `expert` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `designation` varchar(30) NOT NULL,
+  `address` text NOT NULL,
+  `dov` varchar(30) NOT NULL,
+  `pov` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `expert`
+--
+
+INSERT INTO `expert` (`id`, `name`, `designation`, `address`, `dov`, `pov`) VALUES
+(1, 'asdas', 'sadaa', 'asdas', 'sads', 'dsadsa');
 
 -- --------------------------------------------------------
 
@@ -142,6 +164,56 @@ INSERT INTO `rp` (`title`, `pinv`, `status`, `duration`, `budget`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `rpub`
+--
+
+CREATE TABLE `rpub` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `designation` varchar(30) NOT NULL,
+  `dept` varchar(30) NOT NULL,
+  `journal_name` varchar(30) NOT NULL,
+  `title` text NOT NULL,
+  `paperinfo` varchar(30) NOT NULL,
+  `year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rpub`
+--
+
+INSERT INTO `rpub` (`id`, `name`, `designation`, `dept`, `journal_name`, `title`, `paperinfo`, `year`) VALUES
+(1, '', '234234', 'IT', '23423', '', '432423', 423423),
+(2, '', 'Assistant professor', 'IT', 'Springer', '', 'ISBN 11011', 2016),
+(3, '', 'asd', 'IT', 'sad', '', 'sad', 0),
+(4, '', 'Professor', 'IT', 'Springer', '', 'Volume 1', 2015),
+(5, '', 'ww', 'IT', 'qw', '', 'qwq', 11),
+(6, 'Jaswanth', 'Assistant professor', 'IT', 'Cloud Computing', 'Springer', 'al12', 2015);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seminars`
+--
+
+CREATE TABLE `seminars` (
+  `name` varchar(30) NOT NULL,
+  `designation` varchar(30) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `organizer` varchar(30) NOT NULL,
+  `date` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `seminars`
+--
+
+INSERT INTO `seminars` (`name`, `designation`, `title`, `organizer`, `date`) VALUES
+('SVEC', 'Professor', 'CloudComputing', '123', '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -172,10 +244,16 @@ ALTER TABLE `achlist`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `feed`
+-- Indexes for table `events`
 --
-ALTER TABLE `feed`
+ALTER TABLE `events`
   ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `expert`
+--
+ALTER TABLE `expert`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `list`
@@ -196,6 +274,18 @@ ALTER TABLE `rp`
   ADD PRIMARY KEY (`title`);
 
 --
+-- Indexes for table `rpub`
+--
+ALTER TABLE `rpub`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seminars`
+--
+ALTER TABLE `seminars`
+  ADD PRIMARY KEY (`name`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -209,12 +299,22 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `achlist`
 --
 ALTER TABLE `achlist`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT for table `expert`
+--
+ALTER TABLE `expert`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `list`
 --
 ALTER TABLE `list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `rpub`
+--
+ALTER TABLE `rpub`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
